97344db9a69d38a5ce2251b8bb326dcf4508611b
Stewart <stewart.mckinlay@gmail.com>
2021-03-08 13:40:51 -0700

    Introduce IF statements

---
+++ if.py
@@ -0,0 +1,22 @@
+age = 18
+
+if age >= 18:
+    print("You are 18 or older")
+else:
+    print("You are younger than 18")
+
+# This is a single line comment
+
+""" This
+is a 
+multiline 
+comment """
+
+
+year = 1830
+
+if 2000 <= year < 2100:
+    print('Welcome to the 21st century!')
+else:
+    print('You are before or after the 21st century')
+
